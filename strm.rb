503734d805737c4a239e30dce142570443994de1
streammachineio-org <apis@streammachine.io>
2021-10-05 13:08:42 +0000

    Release brew formula for Stream Machine CLI v1.9.2

--- strm.rb
+++ strm.rb
@@ -5,28 +5,28 @@
 class Strm < Formula
   desc "Command Line Interface for https://streammachine.io"
   homepage "https://streammachine.io"
-  version "1.9.1"
+  version "1.9.2"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/streammachineio/cli/releases/download/v1.9.1/strm_darwin_amd64.tar.gz"
-      sha256 "7506ae1e8c313fdbb2cbfb8113ca54723fdb4a3c3f8393f129ab67ca784fbbe0"
+      url "https://github.com/streammachineio/cli/releases/download/v1.9.2/strm_darwin_amd64.tar.gz"
+      sha256 "4f250518a3a550219bd58ae6f0428509d204c545f37c0671bd242bb2cad7d660"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/streammachineio/cli/releases/download/v1.9.1/strm_darwin_arm64.tar.gz"
-      sha256 "a9ec5ff8ec416faff4edbcf276a69382089ff48e2fd065beab30e3f0077e4b18"
+      url "https://github.com/streammachineio/cli/releases/download/v1.9.2/strm_darwin_arm64.tar.gz"
+      sha256 "06b00ed04c9677f81c43c406dd13dc7027b8983054bc2a3d49a8afad78c0a321"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/streammachineio/cli/releases/download/v1.9.1/strm_linux_amd64.tar.gz"
-      sha256 "109816d8d7642eb3bd9a537a9674a1e293ba5f1ed0648c75c9436b1b409d82da"
+      url "https://github.com/streammachineio/cli/releases/download/v1.9.2/strm_linux_amd64.tar.gz"
+      sha256 "c51d456a5b3c7b58045abacded161cae75c8bd966bcb38fff30a75c9e5c8180a"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/streammachineio/cli/releases/download/v1.9.1/strm_linux_arm64.tar.gz"
-      sha256 "f0a415dceb772c060593ea66fa89af0ef62338564f6997e977413ee556e044d1"
+      url "https://github.com/streammachineio/cli/releases/download/v1.9.2/strm_linux_arm64.tar.gz"
+      sha256 "2fbb0d9aa43d8fc68aeb01207c1d1510d5f4d7edf34fb8a8338555d1e7810866"
     end
   end
 
