adaf8591e503b6c139e329414f13e32ef488916f
streammachineio-org <apis@streammachine.io>
2022-09-02 09:49:45 +0000

    Release brew formula for STRM Privacy CLI v2.14.1

--- strm.rb
+++ strm.rb
@@ -5,20 +5,20 @@
 class Strm < Formula
   desc "Command Line Interface for https://strmprivacy.io"
   homepage "https://strmprivacy.io"
-  version "2.14.0"
+  version "2.14.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/strmprivacy/cli/releases/download/v2.14.0/strm_darwin_arm64.tar.gz"
-      sha256 "d98d25224a1d5921be621accbb198f5ee25293145bd673b5c394f4f1e25c12ce"
+      url "https://github.com/strmprivacy/cli/releases/download/v2.14.1/strm_darwin_arm64.tar.gz"
+      sha256 "06a67006957364a57bfcfe3fb2e0365f83f6884eef6d125546b84732c7a7b8d7"
 
       def install
         bin.install "strm"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/strmprivacy/cli/releases/download/v2.14.0/strm_darwin_amd64.tar.gz"
-      sha256 "38c91b8071fc590234228645ae8ae822f5b109ed8cf44373739b276bfc55a3ec"
+      url "https://github.com/strmprivacy/cli/releases/download/v2.14.1/strm_darwin_amd64.tar.gz"
+      sha256 "13d99d671c9d00f8ef96f096d8b2357a51bbc4aa8ac1be60a6eef341a45abc04"
 
       def install
         bin.install "strm"
@@ -27,17 +27,17 @@ class Strm < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/strmprivacy/cli/releases/download/v2.14.0/strm_linux_arm64.tar.gz"
-      sha256 "272ad1376cf061ad05a48369f5ac4f34096ad74a1b3d5eed60dd09359cff9851"
+    if Hardware::CPU.intel?
+      url "https://github.com/strmprivacy/cli/releases/download/v2.14.1/strm_linux_amd64.tar.gz"
+      sha256 "6a9c966948b8b7835d802b4de93fc7c6b0e3c22779e32bd92e49ebc4c1343430"
 
       def install
         bin.install "strm"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/strmprivacy/cli/releases/download/v2.14.0/strm_linux_amd64.tar.gz"
-      sha256 "c00905072cf0f30b323430d529dc9c7011a243fd97d80c2a6a24d48fb52f6a42"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/strmprivacy/cli/releases/download/v2.14.1/strm_linux_arm64.tar.gz"
+      sha256 "769b9fc0b4723d8e35345be4154d3dcc59ff4c16e4eab3541b615a9ee24b718e"
 
       def install
         bin.install "strm"
@@ -45,24 +45,25 @@ class Strm < Formula
     end
   end
 
-  def caveats; <<~EOS
-    In order to set up auto completion, please follow the instructions below. When you've set up auto completion in the past, you can skip these instructions.
+  def caveats
+    <<~EOS
+      In order to set up auto completion, please follow the instructions below. When you've set up auto completion in the past, you can skip these instructions.
 
-     for bash users
-       add the following line to your .bash_profile or .bashrc:
-         source <(strm completion bash)
+       for bash users
+         add the following line to your .bash_profile or .bashrc:
+           source <(strm completion bash)
 
-       to load completions for each session, execute once:
-         - Linux users: strm completion bash > /etc/bash_completion.d/strm
-         - macOS users: strm completion bash > /usr/local/etc/bash_completion.d/strm
+         to load completions for each session, execute once:
+           - Linux users: strm completion bash > /etc/bash_completion.d/strm
+           - macOS users: strm completion bash > /usr/local/etc/bash_completion.d/strm
 
-     for zsh users
-       ensure that shell completion is enabled, then run (only needs to be done once):
-         /bin/zsh -c 'strm completion zsh > "${fpath[1]}/_strm"'
+       for zsh users
+         ensure that shell completion is enabled, then run (only needs to be done once):
+           /bin/zsh -c 'strm completion zsh > "${fpath[1]}/_strm"'
 
-     for fish users
-       strm completion fish > ~/.config/fish/completions/strm.fish # or $XDG_CONFIG_HOME instead of ~/.config
-  EOS
+       for fish users
+         strm completion fish > ~/.config/fish/completions/strm.fish # or $XDG_CONFIG_HOME instead of ~/.config
+    EOS
   end
 
   test do
